ea594b7a98100ce7f380586a625cccb080188dae
berkedursunoglu <berkedursunoglu@gmail.com>
2022-10-28 18:04:06 +0300

    onboarding screen

---
+++ app/src/main/java/com/berkedursunoglu/kapimda/presentation/ui/onboarding/OnboardingSecondFragment.kt
@@ -0,0 +1,53 @@
+package com.berkedursunoglu.kapimda.presentation.ui.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.berkedursunoglu.kapimda.R
+import com.berkedursunoglu.kapimda.databinding.FragmentOnboardingFirstBinding
+import com.berkedursunoglu.kapimda.databinding.FragmentOnboardingSecondBinding
+
+class OnboardingSecondFragment : Fragment() {
+
+    private lateinit var binding: FragmentOnboardingSecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding = FragmentOnboardingSecondBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnForward.setOnClickListener {
+            forwardView(it)
+        }
+
+        binding.btnBack.setOnClickListener {
+            backwardView(it)
+        }
+    }
+
+    private fun forwardView(view: View){
+        val actions = OnboardingSecondFragmentDirections.actionOnboardingSecondFragmentToOnboardingThirdFragment()
+        view.findNavController().navigate(actions)
+    }
+
+    private fun backwardView(view: View){
+        val actions = OnboardingSecondFragmentDirections.actionOnboardingSecondFragmentToOnboardingFirstFragment()
+        view.findNavController().navigate(actions)
+    }
+
+
+}
---
+++ app/src/main/java/com/berkedursunoglu/kapimda/presentation/ui/onboarding/OnboardingThirdFragment.kt
@@ -0,0 +1,44 @@
+package com.berkedursunoglu.kapimda.presentation.ui.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.berkedursunoglu.kapimda.R
+import com.berkedursunoglu.kapimda.databinding.FragmentOnboardingFirstBinding
+import com.berkedursunoglu.kapimda.databinding.FragmentOnboardingSecondBinding
+import com.berkedursunoglu.kapimda.databinding.FragmentOnboardingThirdBinding
+
+
+class OnboardingThirdFragment : Fragment() {
+
+    private lateinit var binding: FragmentOnboardingThirdBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding = FragmentOnboardingThirdBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnBack.setOnClickListener {
+            backwardView(it)
+        }
+    }
+
+    private fun backwardView(view: View){
+        val actions = OnboardingThirdFragmentDirections.actionOnboardingThirdFragmentToOnboardingSecondFragment()
+        view.findNavController().navigate(actions)
+    }
+
+}
