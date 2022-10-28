7bc2ce46eba25b5e64d9bee4112f21f9d81186c7
berkedursunoglu <berkedursunoglu@gmail.com>
2022-10-28 19:17:45 +0300

    splash screen

--- app/src/main/java/com/berkedursunoglu/kapimda/presentation/ui/onboarding/OnboardingSecondFragment.kt
+++ app/src/main/java/com/berkedursunoglu/kapimda/presentation/ui/onboarding/OnboardingSecondFragment.kt
@@ -6,8 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
-import com.berkedursunoglu.kapimda.R
-import com.berkedursunoglu.kapimda.databinding.FragmentOnboardingFirstBinding
 import com.berkedursunoglu.kapimda.databinding.FragmentOnboardingSecondBinding
 
 class OnboardingSecondFragment : Fragment() {
